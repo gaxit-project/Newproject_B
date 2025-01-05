28b13c969395c9ba2ebf29be1056c0cfb010e78d
e1c23007 <e1c23007@oit.ac.jp>
2025-01-05 22:33:05 +0900

    2025/1/5 梅園　二種類の攻撃を追加

--- Assets/Ume/Scripts/BossScript.cs
+++ Assets/Ume/Scripts/BossScript.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
-    public GameObject fishPrefab;
+    [SerializeField] private GameObject fishPrefab;
+    [SerializeField] private GameObject rockPrefab;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float attackInterval = 2f;
+    [SerializeField] private int attackNumber = 2; //攻撃の種類
+    int attackType = 1; //攻撃の種類
+
+
+    private bool isFishAttack = true; //攻撃の切り替えフラグ
     public float fishSpeed = 10;
     private Transform player; //playerの位置を入れる変数
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
     void Awake()
     {
         // タグが"Player"のオブジェクトを探してTransformを取得
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        InvokeRepeating("Attack", attackInterval, attackInterval);
 
     }
 
@@ -24,9 +28,10 @@ public class BossScript : MonoBehaviour
     {
 
         LookAtPlayer();
+
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            fishShot(); //Zキー入力で弾を発射
+            //fishShot(); //Zキー入力で弾を発射
         }
     }
 
@@ -39,9 +44,39 @@ public class BossScript : MonoBehaviour
         }
     }
 
+    void Attack()
+    {
+        GameObject currentAttack;
+
+        switch (attackType)
+        {
+            case 1:
+                currentAttack = fishPrefab;
+                break;
+            case 2:
+                currentAttack = rockPrefab;
+                break;
+
+            default:
+                currentAttack = null;
+                break;
+        }
+
+        if (currentAttack != null)
+        {
+            Instantiate(currentAttack, spawnPoint.position, spawnPoint.rotation);
+        }
+
+        // 攻撃タイプを切り替え
+        attackType++;
+        if (attackType > attackNumber)
+        {
+            attackType = 1; // 循環するようにリセット
+        }
+    }
     private void fishShot()
     {
-        GameObject newFish = Instantiate(fishPrefab, this.transform.position, Quaternion.identity);
+        GameObject newFish = Instantiate(fishPrefab);//, this.transform.position, Quaternion.identity);
         Rigidbody fishRigidbody = newFish.GetComponent<Rigidbody>();
         fishRigidbody.AddForce(this.transform.forward * fishSpeed, ForceMode.Impulse);
         Destroy(newFish, 10); //10秒後に削除
