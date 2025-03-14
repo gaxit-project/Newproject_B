7173b6f164464f7482fd62c3b04ee938043cb87e
yuun9129 <yuun9129@gmail.com>
2025-03-15 02:13:15 +0900

    3/15 2:12 亀井　TabやBoss撃破時に反射ボタン無効

--- Assets/Script/PlayerInputHandler.cs
+++ Assets/Script/PlayerInputHandler.cs
@@ -6,21 +6,26 @@ public class PlayerInputHandler : MonoBehaviour
 {
     private PlayerMovement playerMovement;
     private ShieldController shieldController;
-
+    private BossScript bossScript;
     void Start()
     {
         playerMovement = FindObjectOfType<PlayerMovement>();
         shieldController = FindObjectOfType<ShieldController>();
+        bossScript = FindObjectOfType<BossScript>();
 
         if (playerMovement == null)
-            Debug.LogError("PlayerMovement が見つかりません！");
+            Debug.LogError("PlayerMovement が見つかりませんで！");
         if (shieldController == null)
-            Debug.LogError("ShieldController が見つかりません！");
+            Debug.LogError("ShieldController が見つかりませんで！");
+        if (bossScript == null)
+            Debug.LogError("BossScript が見つかりませんで！");
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown("joystick button 0"))
+        if (Time.timeScale == 0 || (bossScript != null && bossScript.GetBossHP() <= 0)) return; // タイムスケールが 0 または BossのHPが0以下のときは入力を受け付けない
+
+        if ((Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown("joystick button 0")) && !shieldController.IsReflecting())
         {
             playerMovement?.StartDodge();
             shieldController?.TriggerReflect();
